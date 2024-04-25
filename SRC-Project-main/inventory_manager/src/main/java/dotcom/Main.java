6e59b5eb7f43bef72496287144faa05cbc4f6ecc
juno <69171212+3star333@users.noreply.github.com>
2024-04-25 15:52:56 -0700

    Add files via upload

---
+++ SRC-Project-main/inventory_manager/src/main/java/dotcom/Main.java
@@ -0,0 +1,21 @@
+package dotcom;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+        
+     
+
+
+
+        
+    }
+}
---
+++ SRC-Project-main/inventory_manager/src/main/java/dotcom/author.java
@@ -0,0 +1,5 @@
+package dotcom;
+
+public @interface author {
+
+}
