240ae1051714579262fdd7aa50a2161f4fd41950
Hoang Quan Dinh <hoangquandinh0104@gmail.com>
2024-04-26 22:41:59 -0700

    Added javadoc documentation

--- inventory_manager/src/main/java/dotcom/Inventory.java
+++ inventory_manager/src/main/java/dotcom/Inventory.java
@@ -19,15 +19,22 @@ import java.io.FileReader;
 public class Inventory {
     private ArrayList<Produce> items;
 
+    /**
+     * Class constructor, initializes an empty ArrayList
+     */
     public Inventory() {
         items = new ArrayList<>();
     }
 
+    /**
+     * Returns the ArrayList containing the stored product items.
+     * @return	An ArrayList of Produce objects
+     */
     public ArrayList<Produce> getItems() {
         return items;
     }
        
-      /**
+    /**
      * Add a new product into the current ArrayList with the given name, weight and quantity as object fields. automatically generates unique ID
      * @param name		A string containing the name of the product	
      * @param weight	A double containing the weight of one product
@@ -39,7 +46,8 @@ public class Inventory {
         produce.setQuantity(quantity); // Set the quantity
         items.add(produce);
     }
-     /**
+
+    /**
      * Add an amount quantityToAdd to the product in the ArrayList with the specificed produceName.
      * If there's no product with that name in the ArrayList, output an error message and exit the method.
      * @param produceName	A string indicating the name of the product to add the quantity to.
@@ -54,10 +62,10 @@ public class Inventory {
             }
             else;
             System.out.println("Error: Produce item not found in inventory.");
-        }
-        
+        }    
     }
-/**
+
+    /**
      * Remove an amount quantityToRemove to the product in the ArrayList with the indicated produceName.
      * If the quantity to remove is greater than the existing quantity of the product, output an error message and exit the method.
      * If there's no product with that name in the ArrayList, output an error method and exit the method.
@@ -79,6 +87,11 @@ public class Inventory {
         }
         System.out.println("Error: Produce item not found in inventory.");
     }
+
+    /**
+     * Displays the interface for inputting the paramters (name, weight & quantity) of the object to add. 
+     * Once the [Add Produce] button is pressed, calls the addProduce method with the equivalent parameters passed.
+     */
     public void showAddProductDialogue() {
         JFrame frame = new JFrame("Add Produce");
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -137,6 +150,12 @@ public class Inventory {
         frame.setVisible(true);
     }
 
+    /**
+     * Search the stored ArrayList for any Produce with the String name that matches the parameter productName, case insensitive and add it to a created List.
+     * Once all the objects in the stored ArrayList is checked, return the resulting List. If the List is empty, outputs an error message.
+     * @param productName	A String containing the name to search for.
+     * @return		A Produce List containing all the Produce items with the String name that matches the productName.
+     */
     public List<Produce> searchForProducts(String productName) {
         List<Produce> matchingProducts = new ArrayList<>();
         for (Produce product : items) {
@@ -150,6 +169,11 @@ public class Inventory {
         return matchingProducts;
     }
     
+    /**
+     * Print the stored name, weight and quantity of the Produce object being passed in.
+     * If the object is null, outputs an error message.
+     * @param produce	A Produce object containing the data printed.
+     */
     public void showProduceData (Produce produce) {
         if (produce != null) {
             System.out.println("Produce Data: ");
@@ -162,6 +186,12 @@ public class Inventory {
         }
     }
 
+    /**
+     * Returns the string containing the data (name, weight and quantity) of every object in the stored ArrayList.
+     * If the arrayList is empty, returns a "No produce items found in inventory" String instead.
+     * @return	A String either containing the data of each Produce objects in the stored ArrayList formatted
+     * 		or "No produce items found in inventory"
+     */
     public String ShowAllProduceData() {
         StringBuilder output = new StringBuilder();
         if (!items.isEmpty()) {
@@ -176,6 +206,14 @@ public class Inventory {
         }
         return output.toString();
     }
+
+    /**
+     * Writes the data of all the object of the arrayList (through object specific toString()) seperated by new lines
+     * into a file with the specified filename.
+     * If successfully written, outputs a message indicating the data was saved successfully.
+     * If an IOException occurs, outputs an error message with a description of the error.
+     * @param filename	The String containing the name of the file the data is saved to.
+     */
     public void saveInventory(String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
             for (Produce product : items) {
@@ -187,8 +225,15 @@ public class Inventory {
             System.err.println("Error saving inventory data: " + e.getMessage());
         }
     }
+
+    /**
+     * Populate the stored arrayList with Produce objects made up of the data in the file with the specified filename.
+     * The format for the stored data should be [Name],[Quantity],[Weight], each block mapped to their specified dataa and the commas are delimiters.
+     * If the data is read, prints a message indicating the data was saved successfully.
+     * If an IOException occurs, outputs an error message with the description of the error.
+     * @param filename
+     */
     public void loadInventory(String filename) {
-        items = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
--- inventory_manager/src/main/java/dotcom/Produce.java
+++ inventory_manager/src/main/java/dotcom/Produce.java
@@ -4,34 +4,63 @@ public class Produce {
     private int quantity; // New field for quantity
     private double weight;
 
+    /**
+     * Constructor for the class.
+     * @param name		A String for the instance variable containing the name of the Produce.
+     * @param weight	A double for the instance variable containing the weight of the Produce.
+     * @param quantity	An int for the instance variable contaaining the quantity of the Produce.
+     */
     public Produce(String name, double weight, int quantity) {
         this.name = name;
         this.weight = weight;
         this.quantity = quantity;
     }
+
+    /**
+     * Returns the object's stored String name.
+     * @return	A String containing the value of the stored name variable of the object.
+     */
     public String getName() {
         return name;
     }
 
-    public int getQuantity() { // Getter for quantity
+    /**
+     * Returns the object's stored integer quantity.
+     * @return	An int containing the value of the stored quantity variable of the object.
+     */
+    public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) { // Setter for quantity
+    /**
+     * Set the stored quantity variable of the object to the specified value.
+     * @param quantity	An integer that the stored variable quantity will be set to.
+     */
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
     
+    /**
+     * Returns the object's stored double weight.
+     * @return	A double containing the value of the stored weight variable of the object.
+     */
     public double getWeight() {
         return weight;
     }
 
-
+    /**
+     * Returns a string representation of the stored variables of the object.
+     * @return	A string of the value of the stored variables of the object, seperated by commas.
+     */
     @Override
     public String toString() {
         return name + "," + quantity + "," + weight;
     }
 
-    
+    /**
+     * Returns a string representation of the stored variables of the object in the specified format, each variable seperated by a new line char.
+     * @return	A string of the value of the stored variables of the objects, with a header and seperated by new line chars.
+     */
     public String toStringPretty() {
         return "Name: " + name + "\nQuantity: " + quantity + "\nWeight: " + weight +"\n";
     }
