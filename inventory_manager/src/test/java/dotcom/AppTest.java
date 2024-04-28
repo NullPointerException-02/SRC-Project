26f3676cf06ca13e28fc733c5ef507455cf79b76
aliciaxprince <149344703+aliciaxprince@users.noreply.github.com>
2024-04-28 15:40:42 -0700

    initial commit

--- inventory_manager/src/test/java/dotcom/AppTest.java
+++ inventory_manager/src/test/java/dotcom/AppTest.java
@@ -1,18 +1,84 @@
 package dotcom;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-class AppTest {
+class AppTest 
+{
     /**
-     * Rigorous Test.
+     * Tests the addProduce function in the inventory class 
      */
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testInventory_addProduce() 
+    {
+        String name = "apple";
+        double weight = 4.5;
+        int quantity = 9;
+        Inventory testinventory = new Inventory();
+        testinventory.addProduce(name, weight, quantity);
+        ArrayList<Produce> items = testinventory.getItems();
+        assertEquals(items.get(0).getName(), name);
+        assertEquals(items.get(0).getWeight(), weight);
+        assertEquals(items.get(0).getQuantity(), quantity);
+    }
+
+    /**
+     * Tests the addToExistingEntry function in the inventory class 
+     */
+    @Test
+    void testInventory_addToExistingEntry() 
+    {
+        String name = "apple";
+        double weight = 4.5;
+        int quantity = 9;
+        int quantitytoradd = 4;
+        Inventory testinventory = new Inventory();
+        testinventory.addProduce(name, weight, quantity);
+        testinventory.addToExistingEntry(name, quantitytoradd);
+        ArrayList<Produce> items = testinventory.getItems();
+        assertEquals(items.get(0).getName(), name);
+        assertEquals(items.get(0).getWeight(), weight);
+        assertEquals(items.get(0).getQuantity(), (quantity + quantitytoradd));
+    }
+
+    /**
+     * Tests the addToExistingEntry function in the inventory class 
+     */
+    @Test
+    void testInventory_removeQuantity()
+    {
+        String name = "apple";
+        double weight = 4.5;
+        int quantity = 9;
+        int quantitytoremove = 4;
+        Inventory testinventory = new Inventory();
+        testinventory.addProduce(name, weight, quantity);
+        testinventory.removeQuantity(name, quantitytoremove);
+        ArrayList<Produce> items = testinventory.getItems();
+        assertEquals(items.get(0).getName(), name);
+        assertEquals(items.get(0).getWeight(), weight);
+        assertEquals(items.get(0).getQuantity(), (quantity - quantitytoremove));
+    }
+
+    /**
+     * Tests the searchForProducts function in the inventory class 
+     */
+    @Test
+    void testInventory_searchForProducts()
+    {
+        String name = "apple";
+        double weight = 4.5;
+        int quantity = 9;
+        Inventory testinventory = new Inventory();
+        testinventory.addProduce(name, weight, quantity);
+        List<Produce> items = testinventory.searchForProducts(name);
+        assertEquals(items.get(0).getName(), name);
+        assertEquals(items.get(0).getWeight(), weight);
+        assertEquals(items.get(0).getQuantity(), (quantity));
     }
 }
+
